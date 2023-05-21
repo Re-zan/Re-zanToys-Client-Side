abff30871065b53faaa0da3202089528fa48e916
Re-zan <zannat881all@gmail.com>
2023-05-21 15:27:26 +0600

    data connected to the db

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -4,7 +4,7 @@ import BlogData from "../../componentes/Blog/BlogData";
 const Blog = () => {
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
-    fetch("blogs.json")
+    fetch("http://localhost:5000/blogs")
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
@@ -12,7 +12,7 @@ const Blog = () => {
     <div>
       <div className="my_container grid grid-cols-1 md:grid-cols-2 gap-6 p-4 lg:p-0 my-20">
         {blogs.map((blog) => (
-          <BlogData key={blog.id} blog={blog}></BlogData>
+          <BlogData key={blog._id} blog={blog}></BlogData>
         ))}
       </div>
     </div>
--- src/componentes/Blog/Blogs.jsx
+++ src/componentes/Blog/Blogs.jsx
@@ -4,14 +4,14 @@ import BlogData from "./BlogData";
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
-    fetch("blogs.json")
+    fetch("http://localhost:5000/blogs")
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
   return (
     <div className="my_container grid grid-cols-1 md:grid-cols-2 gap-6 p-4 lg:p-0 mb-10">
       {blogs.slice(0, 2).map((blog) => (
-        <BlogData key={blog.id} blog={blog}></BlogData>
+        <BlogData key={blog._id} blog={blog}></BlogData>
       ))}
     </div>
   );
